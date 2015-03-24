eef65a6a6d1e228cb8ac41974f7a123321953743
Karl Benedict <kbene@unm.edu>
2015-03-24 11:26:17 -0600

    updated generate.sh to output the RDAoverview.slides.linked.html

--- generate.sh
+++ generate.sh
@@ -2,4 +2,4 @@ pandoc -o ./generated/TechnicalDebtConceptualModel.md.docx --filter pandoc-citep
 pandoc -o ./web/TechnicalDebtConceptualModel.md.html --filter pandoc-citeproc --standalone --self-contained --css=./web/web.css TechnicalDebtConceptualModel.md
 pandoc -o ./generated/TechnicalDebtConceptualModel.md.pdf  --filter pandoc-citeproc --template=./reference/standardWidth_inline_images.latex TechnicalDebtConceptualModel.md
 
-pandoc -o ./web/TechnicalDebtConceptualModel.md.slides.linked.html -t dzslides --slide-level 3 --standalone --css=slides.css RDAoverview.md
+pandoc -o ./web/RDAoverview.slides.linked.html -t dzslides --slide-level 3 --standalone --css=slides.css RDAoverview.md
